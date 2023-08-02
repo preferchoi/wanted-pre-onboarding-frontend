a765d43222dc898cadcb7a16daa773cfb66b5f06
prefer_choi <prefer_choi@naver.com>
2023-08-02 18:33:44 +0900

    데이터 유효성 체크

--- prefer/src/routes/signup.jsx
+++ prefer/src/routes/signup.jsx
@@ -1,12 +1,36 @@
+import React, { useState, useEffect } from 'react';
 import { Outlet, Link } from "react-router-dom";
 
-export default function signup() {
+export default function Signup() {
+    const [email, setEmail] = useState("");
+    const [PW, setPW] = useState("");
+    const [isEmailValid, setIsEmailValid] = useState(false);
+    const [isPWValid, setIsPWValid] = useState(false);
+
+    const handleEmailChange = (event) => {
+        setEmail(event.target.value);
+    };
+
+    const handlePWChange = (event) => {
+        setPW(event.target.value);
+    };
+
+    useEffect(() => {
+        const emailRegex = /^.*@.*$/;
+        setIsEmailValid(emailRegex.test(email));
+    }, [email]);
+
+    useEffect(() => {
+        const PWRegex = /^.{8,}$/;
+        setIsPWValid(PWRegex.test(PW));
+    }, [PW]);
+
     return (
         <div>
-            <input data-testid="email-input" />
-            <input data-testid="password-input" />
+            <input data-testid="email-input" onChange={handleEmailChange} value={email}/>
+            <input data-testid="password-input" onChange={handlePWChange} value={PW} />
             <Link to={`/signin`}>
-                <button data-testid="signup-button">회원가입</button>
+                <button data-testid="signup-button" disabled={!isEmailValid ^ !isPWValid}>회원가입</button>
             </Link>
         </div>
     );
