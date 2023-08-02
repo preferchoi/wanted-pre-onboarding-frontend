f7564b52e29ce4a4bfe5fcb00f9c3eea4062216c
prefer_choi <prefer_choi@naver.com>
2023-08-02 18:35:07 +0900

    데이터 유효성 체크

--- prefer/src/routes/signin.jsx
+++ prefer/src/routes/signin.jsx
@@ -1,10 +1,34 @@
+import React, { useState, useEffect } from 'react';
+
 export default function Signin() {
+    const [email, setEmail] = useState("");
+    const [PW, setPW] = useState("");
+    const [isEmailValid, setIsEmailValid] = useState(false);
+    const [isPWValid, setIsPWValid] = useState(false);
+
+    const handleEmailChange = (event) => {
+        setEmail(event.target.value);
+    };
+
+    const handlePWChange = (event) => {
+        setPW(event.target.value);
+    };
+
+    useEffect(() => {
+        const emailRegex = /^.*@.*$/;
+        setIsEmailValid(emailRegex.test(email));
+    }, [email]);
+
+    useEffect(() => {
+        const PWRegex = /^.{8,}$/;
+        setIsPWValid(PWRegex.test(PW));
+    }, [PW]);
+
     return (
         <div>
-            <input data-testid="email-input" />
-            <input data-testid="password-input" />
-            <button data-testid="signin-button">로그인</button>
-            
+            <input data-testid="email-input" onChange={handleEmailChange} value={email} />
+            <input data-testid="password-input" onChange={handlePWChange} value={PW}/>
+            <button data-testid="signin-button" disabled={!isEmailValid ^ !isPWValid}>로그인</button>
         </div>
     );
 }
